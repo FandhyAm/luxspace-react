27f41d7480065b345a3736755731a1e455bc7738
FandhyAm <fandhyando01@gmail.com>
2022-08-24 22:37:44 +0700

    modified tailwind and add structuring folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./assets/css/app.css";
 function App() {
   return (
     <div>
-      <h2 className="text-teal-800 text-center">Belajar react tailwind</h2>
+      <h2 className="text-xl">Belajar react tailwind</h2>
     </div>
   );
 }
